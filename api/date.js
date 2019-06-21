833278b6e71850541161aa708568d7faccab7acf
thanhv2 <trieuvuong0168.com>
2019-06-21 22:58:06 +0700

    update my course

--- api/date.js
+++ api/date.js
@@ -1,6 +1,9 @@
 const express = require('express');
 const helmet = require('helmet');
 const moment = require('moment');
+const jwt = require('jsonwebtoken');
+const bcrypt = require('bcryptjs');
+const User = require('./api/User')
 
 const app = express();
 
@@ -11,5 +14,25 @@ app.get('*', (req, res) => {
   const currentTime = moment().format('MMMM Do YYYY, h:mm:ss a');
   res.status(200).send(currentTime);
 });
+
+app.post('/login', function(req, res){
+    var hashedPassword = bcrypt.hashSync(req.body.password, 8);
+
+    User.create({
+        username: req.body.username,
+        password: hashedPassword
+    },
+    function(err, user) {
+        if (err) {
+            return res.status(500).send('There some thing err when register.');}
+        else {
+        var token= jwt.sign({id: user._id}, config.secret, {
+            expiresIn: 86400
+        });
+        res.status(200).send({ auth: true, token: token});
+        }
+    });
+});
+
 
 module.exports = app;
