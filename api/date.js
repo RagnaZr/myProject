4c617a81f9ee97cda8bd9174b8e61f8adbcd3b06
RagnaZr <47023623+RagnaZr@users.noreply.github.com>
2019-06-21 23:49:53 +0700

    Update date.js

--- api/date.js
+++ api/date.js
@@ -3,29 +3,17 @@ const helmet = require('helmet');
 const moment = require('moment');
 const jwt = require('jsonwebtoken');
 const bcrypt = require('bcryptjs');
-
 const mongoose = require('mongoose');
 mongoose.connect("mongodb://localhost/UserDta");
-
 const app = express();
-
 app.use(helmet());
-
 app.get('*', (req, res) => {
   res.set('Content-Type', 'text/html');
   const currentTime = moment().format('MMMM Do YYYY, h:mm:ss a');
-  res.status(200).send(currentTime);
-});
-
-const UsersData = new mongoose.Schema({
-    username: String,
-    password: String
-});
+  res.status(200).send(currentTime);});
+const UsersData = new mongoose.Schema({username: String,password: String});
 mongoose.model('User', UsersData);
-
-app.post('/login', function(req, res){
-    var hashedPassword = bcrypt.hashSync(req.body.password, 8);
-
+app.post('/login', function(req, res){  var hashedPassword = bcrypt.hashSync(req.body.password, 8);
     User.create({
         username: req.body.username,
         password: hashedPassword
@@ -41,6 +29,4 @@ app.post('/login', function(req, res){
         }
     });
 });
-
-
 module.exports = app;
