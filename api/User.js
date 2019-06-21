9a32068de5a46495fa369b374d9dcfacb47ee927
RagnaZr <47023623+RagnaZr@users.noreply.github.com>
2019-06-21 23:41:05 +0700

    Delete User.js

--- api/User.js
+++
@@ -1,8 +0,0 @@
-const mongoose = require('mongoose');
-const UsersData = new mongoose.Schema({
-    username: String,
-    password: String
-});
-mongoose.model('User', UsersData);
-
-module.exports= mongoose.model('User');
